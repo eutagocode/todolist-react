46a69c241111307a6eaf8b5c87a934a704aa3ef2
eutagocode <thiagoluix21@gmail.com>
2024-12-15 11:44:37 -0300

    refactor: :art: Foi movida a parte do item da li para outro arquivo chamado "Item.jsx" para facilitar a manutenção e deixar o código mais limpo e organizado, também foi feita a estilização da lista utilizando a biblioteca TailwindCSS

--- src/List.jsx
+++ src/List.jsx
@@ -1,8 +1,15 @@
+import Item from "./Item";
+
 const List = (props) => {
     return (
-        <ul>
+        <ul className="w-full flex flex-col gap-3 rounded-md p-3">
             {props.items.map((item) => (
-                <li key={item}>{item}</li>
+                <Item
+                    key={item.id}
+                    onDone={props.onDone}
+                    onItemDeleted={props.onItemDeleted}
+                    item={item}
+                ></Item>
             ))}
         </ul>
     );
