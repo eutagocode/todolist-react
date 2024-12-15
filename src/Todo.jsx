098597d1484cbf153d5b930f195253e3c8c329b5
eutagocode <thiagoluix21@gmail.com>
2024-12-15 11:50:19 -0300

    feat: :sparkles: Foram criadas as funções de deletar e colocar o item como feito utilizando as funções onItemDeleted e onDone que é passada para como propriedade de filho para pai

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,19 +1,46 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Item from "./ItemModel";
 import List from "./List";
 import Form from "./Form";
 
 const Todo = () => {
     const [items, setItems] = useState([]);
 
-    const onAddItem = (item) => {
+    const onAddItem = (text) => {
+        const item = new Item(text);
+
         setItems([...items, item]);
     };
 
+    const onItemDeleted = (item) => {
+        const filteredItems = items.filter((task) => task.id !== item.id);
+
+        setItems(filteredItems);
+    };
+
+    const onDone = (item) => {
+        const updatedItems = items.map((task) => {
+            if (task.id === item.id) {
+                task.done = !task.done;
+            }
+            return task;
+        });
+
+        setItems(updatedItems);
+    };
+
     return (
-        <div className="container mx-auto">
-            <h1>Todo</h1>
+        <div className="container mx-auto mt-9 flex flex-col gap-4">
+            <h1 className="text-center font-bold uppercase text-3xl">
+                To Do List
+            </h1>
             <Form onAddItem={onAddItem}></Form>
-            <List items={items}></List>
+            <hr className="h-[1px] w-full bg-slate-300" />
+            <List
+                onDone={onDone}
+                onItemDeleted={onItemDeleted}
+                items={items}
+            ></List>
         </div>
     );
 };
