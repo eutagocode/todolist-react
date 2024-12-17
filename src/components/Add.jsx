30c01889974096b5bc07e6f5ecef3492b7277581
eutagocode <thiagoluix21@gmail.com>
2024-12-17 06:20:35 -0300

    feat: :sparkles: Foi introduzido o novo componente que renderiza o Modal para adicionar os itens

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,9 +1,32 @@
+import { useState } from "react";
+import Modal from "./Modal";
 import plus from "../assets/plus.svg";
 
-const Add = () => {
+const Add = (props) => {
+    const [text, setText] = useState("");
+    const [showModal, setShowModal] = useState(false);
+
+    const handleInput = (event) => {
+        const inputField = event.target.value;
+        setText(inputField);
+    };
+
+    const handleShowModal = (event) => {
+        event.preventDefault();
+        if (text) {
+            props.onAddItem(text);
+        }
+        setShowModal(!showModal);
+
+        setText("");
+    };
+
     return (
         <footer className="w-full flex justify-end">
-            <button className="h-14 w-14 rounded-full bg-violet-500 flex justify-center items-center">
+            <button
+                onClick={handleShowModal}
+                className="h-14 w-14 rounded-full bg-violet-500 flex justify-center items-center"
+            >
                 <img
                     height={32}
                     width={32}
@@ -11,6 +34,12 @@ const Add = () => {
                     alt="Ícone de mais para criar uma nova tarefa"
                 />
             </button>
+            <Modal
+                text={text}
+                handleInput={handleInput}
+                showModal={showModal}
+                handleShowModal={handleShowModal}
+            ></Modal>
         </footer>
     );
 };
