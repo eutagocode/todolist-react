7b08914bd270cca90c5411eb15976cb148995549
eutagocode <thiagoluix21@gmail.com>
2024-12-17 06:13:39 -0300

    refactor: :recycle: Código foi refatorado e foi criado o novo componente CreateItem onde ele renderiza um modal para adicionar um novo item ao clicar no botão de mais no lado direito da tela

---
+++ src/components/CreateItem.jsx
@@ -0,0 +1,25 @@
+const CreateItem = (props) => {
+    return (
+        <form className="w-full mx-4 sm:mx-0 sm:w-1/2 h-60 p-6 bg-white rounded-md flex flex-col justify-center gap-8">
+            <h2 className="font-bold text-2xl uppercase text-center">
+                Adicionar uma nova tarefa
+            </h2>
+            <input
+                onInput={props.handleInput}
+                className="border-b-2 py-2 px-3 hover:border-violet-600 transition-all outline-none"
+                type="text"
+                placeholder="Adicionar uma nova tarefa"
+                value={props.text}
+                autoFocus
+            />
+            <button
+                onClick={props.handleShowModal}
+                className="w-full h-11 bg-violet-500 text-white rounded-md hover:bg-violet-600 transition-colors uppercase font-bold"
+            >
+                Adicionar
+            </button>
+        </form>
+    );
+};
+
+export default CreateItem;
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,20 +1,17 @@
+import CreateItem from "./CreateItem";
+
 const Modal = (props) => {
     return (
-        // Nesta tag deve colocar a className flex para que apareça.
-        <div className="hidden fixed h-screen w-screen top-0 left-0 justify-center items-center bg-black/65">
-            <form className="w-full mx-4 sm:mx-0 sm:w-1/2 h-60 p-6 bg-white rounded-md flex flex-col justify-center gap-8">
-                <h2 className="font-bold text-2xl uppercase text-center">
-                    Adicionar uma nova tarefa
-                </h2>
-                <input
-                    className="border-b-2 py-2 px-3 hover:border-violet-600 transition-all outline-none"
-                    type="text"
-                    placeholder="Adicionar uma nova tarefa"
-                />
-                <button className="w-full h-11 bg-violet-500 text-white rounded-md hover:bg-violet-600 transition-colors uppercase font-bold">
-                    Adicionar
-                </button>
-            </form>
+        <div
+            className={`${
+                props.showModal ? "flex" : "hidden"
+            } fixed h-screen w-screen top-0 left-0 justify-center items-center bg-black/65`}
+        >
+            <CreateItem
+                text={props.text}
+                handleInput={props.handleInput}
+                handleShowModal={props.handleShowModal}
+            ></CreateItem>
         </div>
     );
 };
